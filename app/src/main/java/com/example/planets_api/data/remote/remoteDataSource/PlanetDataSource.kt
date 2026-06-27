9979c87936b14c3e8e839159dc29dac5ef6d4e07
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2026-06-26 22:52:26 -0400

    Feat:  Agrega getCharacter y getCharacterDetail al remote data source de la Api

--- app/src/main/java/com/example/planets_api/data/remote/remoteDataSource/PlanetDataSource.kt
+++ app/src/main/java/com/example/planets_api/data/remote/remoteDataSource/PlanetDataSource.kt
@@ -1,5 +1,7 @@
 package com.example.planets_api.data.remote.remoteDataSource
 
+import com.example.planets_api.data.remote.CharacterDto
+import com.example.planets_api.data.remote.CharacterResponseDto
 import com.example.planets_api.data.remote.PlanetDto
 import com.example.planets_api.data.remote.PlanetsApi
 import com.example.planets_api.data.remote.PlanetsResponseDto
@@ -43,4 +45,46 @@ class PlanetDataSource @Inject constructor(
             return Result.failure(Exception("Error desconocido", e))
         }
     }
+
+    //Character
+
+    suspend fun getCharacters(
+        page: Int,
+        limit: Int,
+        name: String?,
+        gender: String?,
+        race: String?,
+    ): Result<CharacterResponseDto> {
+        try {
+            val response = api.getCharacters(page, limit, name, gender, race)
+            if (!response.isSuccessful) {
+                return Result.failure(Exception("Error de red ${response.code()}"))
+            }
+
+            return Result.success(response.body()!!)
+        } catch (e: HttpException) {
+            return Result.failure(Exception("Error de servidor", e))
+        } catch (e: Exception) {
+            return Result.failure(Exception("Error desconocido", e))
+        }
+    }
+
+    suspend fun getCharacterDetail(id: Int): Result<CharacterDto>{
+        try {
+            val response = api.getCharacterDetail(id)
+
+            if (!response.isSuccessful) {
+                return Result.failure(Exception("Error de red ${response.code()}"))
+            }
+            return Result.success(response.body()!!)
+        }catch (e: HttpException){
+            return Result.failure(Exception("Error de servidor", e))
+        }catch (e: Exception){
+            return Result.failure(Exception("Error desconocido",e))
+        }
+    }
+
+
 }
+
+
