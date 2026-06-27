16f86fe80c3b29b2169266a84a8c8c8aca3fb1d5
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2026-06-26 23:27:19 -0400

    Feat: Agrega implementacion del repositorio Character

---
+++ app/src/main/java/com/example/planets_api/data/repository/CharacterRepositoryImp.kt
@@ -0,0 +1,45 @@
+package com.example.planets_api.data.repository
+
+import com.example.planets_api.data.remote.Resource
+import com.example.planets_api.data.remote.remoteDataSource.PlanetDataSource
+import com.example.planets_api.domain.Character.Repository.CharacterRepository
+import com.example.planets_api.domain.Character.model.Character
+import com.example.planets_api.domain.planets.model.Planets
+import com.example.planets_api.domain.planets.repository.PlanetRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class CharacterRepositoryImp @Inject constructor(
+    private val remoteDataSource: PlanetDataSource
+): CharacterRepository {
+    override suspend fun getCharacters(
+        page: Int,
+        limi: Int,
+        name: String?,
+        gender: String?,
+        race: String?
+    ): Flow<Resource<List<Character>>> = flow{
+      emit(Resource.Loading())
+
+        val response=remoteDataSource.getCharacters(page,limi,name,gender,race)
+        response.onSuccess { characters->
+            emit(Resource.Success(characters.items.map { it.toDomain() }))
+        }.onFailure {
+            emit(Resource.Error(it.message ?: "Error desconocido."))
+        }
+    }
+
+
+    override suspend fun getCharacterDetail(id: Int): Flow<Resource<Character>> = flow {
+    emit(Resource.Loading())
+
+        val response=remoteDataSource.getCharacterDetail(id)
+        response.onSuccess { character->
+            emit(Resource.Success(character.toDomain()))
+        }.onFailure {
+            emit(Resource.Error(it.message ?: "Error desconocido"))
+        }
+    }
+
+}
