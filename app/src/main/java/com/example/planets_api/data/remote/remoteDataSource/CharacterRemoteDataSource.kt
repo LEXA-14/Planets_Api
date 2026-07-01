5e83cd16f8804362da596c32dc2f83132a4d5415
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2026-07-01 10:34:04 -0400

    Refactor: Mueve Character y sus get a un data source aparte

--- app/src/main/java/com/example/planets_api/data/remote/remoteDataSource/CharacterRemoteDataSource.kt
+++ app/src/main/java/com/example/planets_api/data/remote/remoteDataSource/CharacterRemoteDataSource.kt
@@ -2,28 +2,26 @@ package com.example.planets_api.data.remote.remoteDataSource
 
 import com.example.planets_api.data.remote.CharacterDto
 import com.example.planets_api.data.remote.CharacterResponseDto
-import com.example.planets_api.data.remote.PlanetDto
 import com.example.planets_api.data.remote.PlanetsApi
-import com.example.planets_api.data.remote.PlanetsResponseDto
 import retrofit2.HttpException
 import javax.inject.Inject
 
-class PlanetDataSource @Inject constructor(
+class CharacterRemoteDataSource @Inject constructor(
     private val api: PlanetsApi
 ) {
-
-    suspend fun getPlanet(
+    suspend fun getCharacters(
         page: Int,
         limit: Int,
         name: String?,
-        isDestroyed: Boolean?
-
-    ): Result<PlanetsResponseDto> {
+        gender: String?,
+        race: String?,
+    ): Result<CharacterResponseDto> {
         try {
-            val response = api.getPlanets(page, limit, name, isDestroyed)
+            val response = api.getCharacters(page, limit, name, gender, race)
             if (!response.isSuccessful) {
                 return Result.failure(Exception("Error de red ${response.code()}"))
             }
+
             return Result.success(response.body()!!)
         } catch (e: HttpException) {
             return Result.failure(Exception("Error de servidor", e))
@@ -32,23 +30,18 @@ class PlanetDataSource @Inject constructor(
         }
     }
 
-    suspend fun getPlanetDetail(id: Int): Result<PlanetDto> {
+    suspend fun getCharacterDetail(id: Int): Result<CharacterDto>{
         try {
-            val response = api.getPlanetDetail(id)
+            val response = api.getCharacterDetail(id)
+
             if (!response.isSuccessful) {
                 return Result.failure(Exception("Error de red ${response.code()}"))
             }
             return Result.success(response.body()!!)
-        } catch (e: HttpException) {
+        }catch (e: HttpException){
             return Result.failure(Exception("Error de servidor", e))
-        } catch (e: Exception) {
-            return Result.failure(Exception("Error desconocido", e))
+        }catch (e: Exception){
+            return Result.failure(Exception("Error desconocido",e))
         }
     }
-
-
-
-
 }
-
-
