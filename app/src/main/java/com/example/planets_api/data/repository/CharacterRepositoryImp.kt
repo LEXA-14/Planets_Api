79011ee9b287d4ff2782570902f1d7b6100348dd
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2026-07-01 10:54:23 -0400

    Feat: Agrega preview de Screen planets

--- app/src/main/java/com/example/planets_api/data/repository/CharacterRepositoryImp.kt
+++ app/src/main/java/com/example/planets_api/data/repository/CharacterRepositoryImp.kt
@@ -1,6 +1,7 @@
 package com.example.planets_api.data.repository
 
 import com.example.planets_api.data.remote.Resource
+import com.example.planets_api.data.remote.remoteDataSource.CharacterRemoteDataSource
 import com.example.planets_api.data.remote.remoteDataSource.PlanetDataSource
 import com.example.planets_api.domain.Character.Repository.CharacterRepository
 import com.example.planets_api.domain.Character.model.Character
@@ -10,7 +11,7 @@ import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class CharacterRepositoryImp @Inject constructor(
-    private val remoteDataSource: PlanetDataSource
+    private val remoteDataSource: CharacterRemoteDataSource
 ): CharacterRepository {
     override suspend fun getCharacters(
         page: Int,
