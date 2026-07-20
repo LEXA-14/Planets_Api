fe431727a9c22e288985be73b28f8085bba00b1f
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2026-07-20 16:25:54 -0400

    Feat:Crea MainNavigationDisplay.kt implementando navegacion 3 (Backstack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -109,12 +109,12 @@ dependencies {
 // Retrofit
     implementation(libs.retrofit)
     implementation(libs.retrofit2.kotlinx.serialization.converter)
-    implementation(libs.kotlinx.serialization.json)
     implementation(libs.squareup.moshi.kotlin)
     implementation(libs.converter.moshi)
 //photo
     implementation(libs.coil.compose)
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.navigation3.ui)
+    implementation(libs.kotlinx.serialization.json)
 
 }
---
+++ app/src/main/java/com/example/planets_api/Navegacion/MainNavigationDisplay.kt
@@ -0,0 +1,65 @@
+package com.example.planets_api.Navegacion
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.ui.NavDisplay
+import com.example.planets_api.presentacion.Character.Detail.CharacterDetailScreen
+import com.example.planets_api.presentacion.Character.List.ListScreenCharacter
+import com.example.planets_api.presentacion.Planets.detailPlanets.PlanetDetailScreen
+import com.example.planets_api.presentacion.Planets.listPlanet.ListScreen
+
+
+@Composable
+fun MainNavigationDisplay(){
+    val backStack= rememberNavBackStack(Screen.ListPlanet)
+
+    NavDisplay(
+        backStack=backStack,
+        modifier = Modifier.fillMaxSize(),
+        entryProvider = entryProvider {
+
+            entry<Screen.ListPlanet>{
+                ListScreen(
+                    onPlanetClick = { id ->
+                        backStack.add(Screen.DetailPlanet(id))
+                    },
+                    onNavigateToCharacters = { backStack.add(Screen.ListCharacter) },
+                )
+            }
+
+            entry<Screen.DetailPlanet> {key->
+                PlanetDetailScreen(
+                    planetId = key.id,
+                    onBack = {
+                        if (backStack.isNotEmpty()) {
+                            backStack.removeAt(backStack.size - 1)
+                        }
+                    },
+
+                )
+            }
+            entry<Screen.ListCharacter>{
+                ListScreenCharacter(
+                    onCharacterClick = {id->
+                        backStack.add(Screen.DetailCharacter(id))
+                    }
+
+                )
+            }
+            entry<Screen.DetailCharacter> {
+                key->
+                CharacterDetailScreen(
+                    characterId=key.id,
+                    onBack={
+                        if(backStack.isNotEmpty()) backStack.removeAt(backStack.size-1)
+                    }
+                )
+            }
+        }
+    )
+
+
+}
